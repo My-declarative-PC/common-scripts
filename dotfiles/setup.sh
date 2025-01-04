4b60561056259437f560d3165bb08caf21a40f5a
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-04 11:20:11 +0300

    Прототип скрипта установки dot-файлов

---
+++ dotfiles/setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sudo git clone https://github.com/My-declarative-PC/dotfiles.git /etc/dotfiles
+cd /etc/dotfiles
+
+declare -a modules=(
+  "bashrc"
+  "btop"
+  "dunst"
+  "fastfetch"
+  "fish"
+  "git"
+  "helix"
+  "i3"
+  "rofi"
+  "starship"
+  "wezterm"
+  "zellij"
+)
+
+for module in "${modules[@]}"
+do
+  echo "$module"
+  sudo git submodule init          --   "$module"
+  sudo git submodule update --init --   "$module"
+       stow --adopt --dotfiles -t ~     "$module"
+  sudo stow --adopt --dotfiles -t /root "$module"
+done
+
+sudo git checkout -- .
+sudo git clean -fd
+sudo git submodule foreach 'git checkout -- . && git clean -fd'
+
+module="profile"
+echo "$module"
+sudo git submodule init          -- "$module"
+sudo git submodule update --init -- "$module"
+sudo ln -s /etc/dotfiles/$module/custom_init.sh    /etc/profile.d/custom_init.sh
+sudo ln -s /etc/dotfiles/$module/custom_profile.sh /etc/profile.d/custom_profile.sh
