53535d8b9975a115397ae5e98f437c21762bd667
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-04 11:39:59 +0300

    Чуть поменял порядок

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -3,6 +3,14 @@
 sudo git clone https://github.com/My-declarative-PC/dotfiles.git /etc/dotfiles
 cd /etc/dotfiles
 
+module="profile"
+echo "$module"
+sudo git submodule init          -- "$module"
+sudo git submodule update --init -- "$module"
+sudo ln -s /etc/dotfiles/$module/custom_init.sh    /etc/profile.d/custom_init.sh
+sudo ln -s /etc/dotfiles/$module/custom_profile.sh /etc/profile.d/custom_profile.sh
+source /etc/profile.d/custom_profile.sh
+
 declare -a modules=(
   "bashrc"
   "btop"
@@ -24,16 +32,9 @@ do
   sudo git submodule init          --   "$module"
   sudo git submodule update --init --   "$module"
        stow --adopt --dotfiles -t ~     "$module"
-  sudo stow --adopt --dotfiles -t /root "$module"
+  sudo bash -c "source /etc/profile.d/custom_profile.sh && stow --adopt --dotfiles -t /root '${module}'"
 done
 
 sudo git checkout -- .
 sudo git clean -fd
 sudo git submodule foreach 'git checkout -- . && git clean -fd'
-
-module="profile"
-echo "$module"
-sudo git submodule init          -- "$module"
-sudo git submodule update --init -- "$module"
-sudo ln -s /etc/dotfiles/$module/custom_init.sh    /etc/profile.d/custom_init.sh
-sudo ln -s /etc/dotfiles/$module/custom_profile.sh /etc/profile.d/custom_profile.sh
