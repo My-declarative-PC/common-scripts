642a105e0490c5429facc9ca29d91e71ba4d72e0
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-04 10:50:03 +0300

    Вынес команду установки переменных окружения

--- brew/setup.sh
+++ brew/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+bash common/brew/setenv.sh
 
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install gcc
 
 bash common/brew/cli_tools.sh
