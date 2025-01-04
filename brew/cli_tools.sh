51a1ceb7cb98a99c757505ed10d09db331361816
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-04 10:21:11 +0300

    Добавил скроипты установки HomeBrew

---
+++ brew/cli_tools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+brew install  \
+    btop      \
+    dust      \
+    eza       \
+    fastfetch \
+    fd        \
+    fish      \
+    fzf       \
+    gum       \
+    helix     \
+    lazygit   \
+    node      \
+    pnpm      \
+    ripgrep   \
+    sd        \
+    starship  \
+    stow      \
+    typos-lsp \
+    yazi      \
+    yq        \
+    zellij    \
+    zoxide
---
+++ brew/docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brew install       \
+    colima         \
+    docker         \
+    docker-compose \
+    docker-completion
