b21d548d4733a75a897db73b49f7f97f8a252caf
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-02-04 22:06:48 +0300

    внёс список устанавливаемых LSP в общие скрипты

---
+++ pnpm/lsp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pnpm i -g \
+  bash-language-server \
+  intelephense \
+  vscode-langservers-extracted \
+  typescript \
+  typescript-language-server \
+  dockerfile-language-server-nodejs \
+  @microsoft/compose-language-service \
+  helix-snippets-ls
