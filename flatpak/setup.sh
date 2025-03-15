85310158445d5286ee54b6b9e50317e579616987
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-03-15 12:26:30 +0300

    исправил список пакетов

--- flatpak/setup.sh
+++ flatpak/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 flatpak --user override --filesystem=home
+flatpak --user override --filesystem=host
+flatpak --user override --filesystem=host-etc
 flatpak --user override --filesystem=$HOME/.icons
 flatpak --user override --filesystem=$HOME/.themes
 flatpak --user override --filesystem=$HOME/.config
@@ -11,12 +13,12 @@ flatpak --user override --talk-name=org.freedesktop.Notifications
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 flatpak install --assumeyes flathub app.zen_browser.zen
-flatpak install --assumeyes flathub net.cozic.joplin_desktop
-flatpak install --assumeyes flathub com.brave.Browser
 flatpak install --assumeyes flathub com.bitwarden.desktop
-flatpak install --assumeyes flathub it.mijorus.gearlever
-flatpak install --assumeyes flathub org.telegram.desktop
-flatpak install --assumeyes flathub org.wezfurlong.wezterm
+flatpak install --assumeyes flathub com.brave.Browser
 flatpak install --assumeyes flathub com.github.PintaProject.Pinta
-flatpak install --assumeyes flathub org.dbgate.DbGate
+flatpak install --assumeyes flathub com.github.tchx84.Flatseal
 flatpak install --assumeyes flathub com.obsproject.Studio
+flatpak install --assumeyes flathub it.mijorus.gearlever
+flatpak install --assumeyes flathub net.cozic.joplin_desktop
+flatpak install --assumeyes flathub org.dbgate.DbGate
+flatpak install --assumeyes flathub org.telegram.desktop
