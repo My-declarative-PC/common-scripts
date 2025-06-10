db65895ad7d670692532282ec3c0aa7d63b16495
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-06-10 18:04:58 +0300

    перевёл на юсер центричтое установку flatpak

--- flatpak/setup.sh
+++ flatpak/setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak install --assumeyes flathub app.zen_browser.zen
-flatpak install --assumeyes flathub com.bitwarden.desktop
-flatpak install --assumeyes flathub com.brave.Browser
-flatpak install --assumeyes flathub com.github.PintaProject.Pinta
-flatpak install --assumeyes flathub com.github.tchx84.Flatseal
-flatpak install --assumeyes flathub com.obsproject.Studio
-flatpak install --assumeyes flathub io.github.nokse22.minitext
-flatpak install --assumeyes flathub it.mijorus.gearlever
-flatpak install --assumeyes flathub net.cozic.joplin_desktop
-flatpak install --assumeyes flathub org.dbgate.DbGate
-flatpak install --assumeyes flathub org.telegram.desktop
-flatpak install --assumeyes flathub space.rirusha.Cassette
+flatpak --user install --assumeyes flathub app.zen_browser.zen
+flatpak --user install --assumeyes flathub com.bitwarden.desktop
+flatpak --user install --assumeyes flathub com.brave.Browser
+flatpak --user install --assumeyes flathub com.github.PintaProject.Pinta
+flatpak --user install --assumeyes flathub com.github.tchx84.Flatseal
+flatpak --user install --assumeyes flathub com.obsproject.Studio
+flatpak --user install --assumeyes flathub io.github.nokse22.minitext
+flatpak --user install --assumeyes flathub it.mijorus.gearlever
+flatpak --user install --assumeyes flathub net.cozic.joplin_desktop
+flatpak --user install --assumeyes flathub org.dbgate.DbGate
+flatpak --user install --assumeyes flathub org.telegram.desktop
+flatpak --user install --assumeyes flathub space.rirusha.Cassette
