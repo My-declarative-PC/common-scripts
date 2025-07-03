6896abaa30d52840e57bc2e85227b7ba0d7064f0
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-07-03 14:38:58 +0300

    добавил программи журналистов

--- flatpak/setup.sh
+++ flatpak/setup.sh
@@ -9,6 +9,7 @@ flatpak --user install --assumeyes flathub com.brave.Browser
 flatpak --user install --assumeyes flathub com.github.PintaProject.Pinta
 flatpak --user install --assumeyes flathub com.github.tchx84.Flatseal
 flatpak --user install --assumeyes flathub com.obsproject.Studio
+flatpak --user install --assumeyes flathub io.anytype.anytype
 flatpak --user install --assumeyes flathub io.github.nokse22.minitext
 flatpak --user install --assumeyes flathub it.mijorus.gearlever
 flatpak --user install --assumeyes flathub net.cozic.joplin_desktop
