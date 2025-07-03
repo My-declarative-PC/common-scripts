70735315cae69d129eedee583cebbb78ca37a09d
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-07-03 14:37:15 +0300

    добавил альтернативные директории
    - иконок
    - темы

--- flatpak/env.sh
+++ flatpak/env.sh
@@ -5,6 +5,8 @@ flatpak --user override --filesystem=host
 flatpak --user override --filesystem=host-etc
 flatpak --user override --filesystem=$HOME/.local/share/icons
 flatpak --user override --filesystem=$HOME/.local/share/themes
+flatpak --user override --filesystem=$HOME/.icons
+flatpak --user override --filesystem=$HOME/.themes
 flatpak --user override --filesystem=$HOME/.config
 flatpak --user override --env=GTK_THEME=$GTK_THEME
 flatpak --user override --env=GTK_USE_PORTAL=1
