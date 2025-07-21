909dd8bb1bd0401838ad95de4cc29559b7117cd8
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-07-21 08:26:31 +0300

    перенёс заботу о gtk 4 на chezmoi

--- flatpak/env.sh
+++ flatpak/env.sh
@@ -12,20 +12,3 @@ flatpak --user override --env=GTK_THEME=$GTK_THEME
 flatpak --user override --env=GTK_USE_PORTAL=1
 flatpak --user override --env=XCURSOR_THEME=$XCURSOR_THEME
 flatpak --user override --talk-name=org.freedesktop.Notifications
-
-# mkdir -p $HOME/.local/share
-# cp -r /usr/share/icons $HOME/.local/share/icons
-# cp -r /usr/share/themes $HOME/.local/share/themes
-# rm -rf ~/.local/share/icons/default
-# ln -s ~/.local/share/icons/$XCURSOR_THEME ~/.local/share/icons/default
-# sudo rm -rf /usr/share/icons/default
-# sudo ln -s /usr/share/icons/$XCURSOR_THEME /usr/share/icons/default
-
-# export THEME_DIR="$HOME/.local/share/themes/$GTK_THEME"
-
-# # Optionally, add support for libadwaita
-# rm -rf "${HOME}/.config/gtk-4.0" &&
-#     mkdir -p "${HOME}/.config/gtk-4.0" &&
-#     ln -sf "${THEME_DIR}/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/assets" &&
-#     ln -sf "${THEME_DIR}/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/gtk.css" &&
-#     ln -sf "${THEME_DIR}/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/gtk-dark.css"
