600a34146315e1137f9a2c33630df93e8e640f82
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-11-24 16:16:59 +0300

    dobawil gruppirowku

--- flatpak/setup.sh
+++ flatpak/setup.sh
@@ -2,22 +2,26 @@
 
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak --user install --assumeyes flathub app.zen_browser.zen
-flatpak --user install --assumeyes flathub be.alexandervanhee.gradia
 flatpak --user install --assumeyes flathub com.bitwarden.desktop
-flatpak --user install --assumeyes flathub com.brave.Browser
-flatpak --user install --assumeyes flathub com.github.PintaProject.Pinta
 flatpak --user install --assumeyes flathub com.github.tchx84.Flatseal
 flatpak --user install --assumeyes flathub com.github.tenderowl.frog
-flatpak --user install --assumeyes flathub com.obsproject.Studio
 flatpak --user install --assumeyes flathub io.anytype.anytype
 flatpak --user install --assumeyes flathub io.github.nokse22.minitext
 flatpak --user install --assumeyes flathub it.mijorus.gearlever
 flatpak --user install --assumeyes flathub net.cozic.joplin_desktop
 flatpak --user install --assumeyes flathub org.dbgate.DbGate
 flatpak --user install --assumeyes flathub org.onlyoffice.desktopeditors
-flatpak --user install --assumeyes flathub org.pulseaudio.pavucontrol
 flatpak --user install --assumeyes flathub org.remmina.Remmina
+# Internet
+flatpak --user install --assumeyes flathub app.zen_browser.zen
+flatpak --user install --assumeyes flathub com.brave.Browser
 flatpak --user install --assumeyes flathub org.telegram.desktop
+# Multymedia
+flatpak --user install --assumeyes flathub com.obsproject.Studio
+flatpak --user install --assumeyes flathub org.pulseaudio.pavucontrol
 flatpak --user install --assumeyes flathub org.videolan.VLC
 flatpak --user install --assumeyes flathub space.rirusha.Cassette
+# Graphics
+flatpak --user install --assumeyes flathub be.alexandervanhee.gradia
+flatpak --user install --assumeyes flathub com.github.PintaProject.Pinta
+flatpak --user install --assumeyes flathub com.infinipaint.infinipaint
