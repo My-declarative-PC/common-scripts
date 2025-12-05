6699089d6c1d9f08f6bc8e04f773867392bf174a
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-12-06 00:38:06 +0300

    Удалил LSP
    которые теперь занесены в distrobox дпя PHP

--- pnpm/lsp.sh
+++ pnpm/lsp.sh
@@ -2,14 +2,7 @@
 
 pnpm i -g \
     bash-language-server \
-    @biomejs/biome \
     dockerfile-language-server-nodejs \
-    @fsouza/prettierd \
     helix-snippets-ls \
-    intelephense \
     @microsoft/compose-language-service \
-    prettier \
-    typescript \
-    typescript-language-server \
-    vscode-langservers-extracted \
     yaml-language-server@next
