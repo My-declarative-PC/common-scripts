6703556bbc795ce739b892ac9686f8f49889070e
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2026-03-16 15:20:20 +0300

    добавил установку правил

---
+++ uv/desktop.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo wget https://raw.githubusercontent.com/qtile/qtile/refs/heads/master/resources/qtile.desktop \
+    -O /usr/share/xsessions/qtile.desktop
---
+++ uv/udev-rule.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo wget https://raw.githubusercontent.com/qtile/qtile/refs/heads/master/resources/99-qtile.rules \
+    -O /etc/udev/rules.d/99-qtile.rules
